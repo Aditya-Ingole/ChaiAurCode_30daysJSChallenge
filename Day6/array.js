16fdd30d3bc23e345dedb969450696c37a95ec27
Aditya-Ingole <a13ingole@gmail.com>
2024-07-22 15:05:46 +0530

    Day 6 completed!

---
+++ Day6/array.js
@@ -0,0 +1,91 @@
+// *************** Activity 1 ************************ 
+
+// let arr = [1,2,3,4,5];
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[4]);
+
+
+// *************** Activity 2 ************************ 
+
+// let arr = [1,2,3,4,5]
+// console.log(arr);
+// console.log(arr.push(10));
+// console.log(arr);
+// console.log(arr.pop());
+// console.log(arr);
+// console.log(arr.unshift(20));
+// console.log(arr);
+// console.log(arr.shift(20));
+// console.log(arr);
+
+
+// *************** Activity 3 ************************ 
+
+// // original array 
+// let number = [1,2,3,4,5]
+
+// // function to double a number 
+// function double(num) {
+//     return num*2;
+// }
+
+// // map is used to create new array with the operation 
+
+// let arr2 = number.map(double);
+
+// console.log(arr2);
+
+
+
+// // original array 
+// let number = [1,2,3,4,5,6,7,8,9,10];
+
+// function isEven(num) {
+//     return num % 2 == 0;
+// } 
+
+
+// let evenNumber = number.filter(isEven);
+
+// console.log(evenNumber);
+
+
+
+// // Original array
+// let numbers = [1, 2, 3, 4, 5];
+
+// // Calculating the sum of all numbers using reduce
+// let sum = numbers.reduce((total, num) => total + num, 0);
+
+// // Logging the result
+// console.log("The sum of all numbers:", sum);
+
+
+// *************** Activity 4 ************************ 
+
+
+// let a = [1,2,3,4,5]
+
+// for(i = 0; i < a.length; i++){
+//     console.log(a[i])
+// }
+
+
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+
+// arr.forEach(function(n){
+//     console.log(n);
+// })
+
+// *************** Activity 5 ************************ 
+
+let matrix = [ 
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+console.log(matrix);
+
+console.log(matrix[2][2]);
